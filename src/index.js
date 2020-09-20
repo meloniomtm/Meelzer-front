aa5ab82c751fc25545d6a346cbd0dfaf863a49f9
Melissa Tammy <mel.tammy2@gmail.com>
2020-09-19 22:30:22 -0300

    apagando firebase

--- src/index.js
+++ src/index.js
@@ -3,18 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import firebase from 'firebase/app'
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDvRyyEACBlrsyA0FYhhU_t83T5l_qnhsU",
-  authDomain: "meelzer-mtm.firebaseapp.com",
-  databaseURL: "https://meelzer-mtm.firebaseio.com",
-  projectId: "meelzer-mtm",
-  storageBucket: "meelzer-mtm.appspot.com",
-  messagingSenderId: "568952923005",
-  appId: "1:568952923005:web:8a8cd84bfad9e5a79b2214"
-};
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
